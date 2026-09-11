30b7e0d12e52160ba21230ebde92c57a29fefb56
Sierra Gillman <52217109+sjgillman@users.noreply.github.com>
2026-09-11 11:55:12 -0700

    updated code to include libraries and longer iters

--- DataAnalysis/SubsettingModelOutputs.R
+++ DataAnalysis/SubsettingModelOutputs.R
@@ -3,7 +3,9 @@
 ### Code by S.J. Gillman
 
 # Code used to pull out parts of results and save in smaller chunks due to github size limitations
-
+library(tidyverse)
+library(MCMCvis)
+library(coda)
 
 # Model outputs from hyak
 load("RESULTS/CH1_nSQ_results_1.RData")
@@ -124,14 +126,59 @@ saveRDS(lambda_sub23, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof6c.rds")
 saveRDS(lambda_sub24, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof6d.rds")
 
 
-# Subset for Expected Group Size
-gs_sub <- post_gof %>%
-  as.data.frame() %>%
-  dplyr::select(contains("gs.lam")) %>%
-  as.matrix()
+lambda_sub25 <- lambda_sub[6001:7000,1:2000]
+lambda_sub26 <- lambda_sub[6001:7000,2001:4000]
+lambda_sub27 <- lambda_sub[6001:7000,4001:6000]
+lambda_sub28 <- lambda_sub[6001:7000,6001:8095]
+
+saveRDS(lambda_sub25, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof7.rds")
+saveRDS(lambda_sub26, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof7b.rds")
+saveRDS(lambda_sub27, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof7c.rds")
+saveRDS(lambda_sub28, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof7d.rds")
 
 
-saveRDS(gs_sub, "RESULTS/COMPRESSED/GOF/gs_posterior_gof.rds")
+
+lambda_sub29 <- lambda_sub[7001:8000,1:2000]
+lambda_sub30 <- lambda_sub[7001:8000,2001:4000]
+lambda_sub31 <- lambda_sub[7001:8000,4001:6000]
+lambda_sub32 <- lambda_sub[7001:8000,6001:8095]
+
+saveRDS(lambda_sub29, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof8.rds")
+saveRDS(lambda_sub30, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof8b.rds")
+saveRDS(lambda_sub31, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof8c.rds")
+saveRDS(lambda_sub32, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof8d.rds")
+
+
+lambda_sub33 <- lambda_sub[8001:9000,1:2000]
+lambda_sub34 <- lambda_sub[8001:9000,2001:4000]
+lambda_sub35 <- lambda_sub[8001:9000,4001:6000]
+lambda_sub36 <- lambda_sub[8001:9000,6001:8095]
+
+saveRDS(lambda_sub33, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof9.rds")
+saveRDS(lambda_sub34, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof9b.rds")
+saveRDS(lambda_sub35, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof9c.rds")
+saveRDS(lambda_sub36, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof9d.rds")
+
+lambda_sub37 <- lambda_sub[9001:10000,1:2000]
+lambda_sub38 <- lambda_sub[9001:10000,2001:4000]
+lambda_sub39 <- lambda_sub[9001:10000,4001:6000]
+lambda_sub40 <- lambda_sub[9001:10000,6001:8095]
+
+saveRDS(lambda_sub37, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof10.rds")
+saveRDS(lambda_sub38, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof10b.rds")
+saveRDS(lambda_sub39, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof10c.rds")
+saveRDS(lambda_sub40, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof10d.rds")
+
+
+lambda_sub41 <- lambda_sub[10001:10200,1:2000]
+lambda_sub42 <- lambda_sub[10001:10200,2001:4000]
+lambda_sub43 <- lambda_sub[10001:10200,4001:6000]
+lambda_sub44 <- lambda_sub[10001:10200,6001:8095]
+
+saveRDS(lambda_sub41, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof11.rds")
+saveRDS(lambda_sub42, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof11b.rds")
+saveRDS(lambda_sub43, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof11c.rds")
+saveRDS(lambda_sub44, "RESULTS/COMPRESSED/GOF/lambda_posterior_gof11d.rds")
 
 # Subset for detections
 pcap_sub <- post_gof %>%
@@ -201,3 +248,58 @@ saveRDS(pcap_sub21, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof6.rds")
 saveRDS(pcap_sub22, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof6b.rds")
 saveRDS(pcap_sub23, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof6c.rds")
 saveRDS(pcap_sub24, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof6d.rds")
+
+pcap_sub25 <- pcap_sub[6001:7000,1:2000]
+pcap_sub26 <- pcap_sub[6001:7000,2001:4000]
+pcap_sub27 <- pcap_sub[6001:7000,4001:6000]
+pcap_sub28 <- pcap_sub[6001:7000,6001:8095]
+
+saveRDS(pcap_sub25, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof7.rds")
+saveRDS(pcap_sub26, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof7b.rds")
+saveRDS(pcap_sub27, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof7c.rds")
+saveRDS(pcap_sub28, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof7d.rds")
+
+
+
+pcap_sub29 <- pcap_sub[7001:8000,1:2000]
+pcap_sub30 <- pcap_sub[7001:8000,2001:4000]
+pcap_sub31 <- pcap_sub[7001:8000,4001:6000]
+pcap_sub32 <- pcap_sub[7001:8000,6001:8095]
+
+saveRDS(pcap_sub29, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof8.rds")
+saveRDS(pcap_sub30, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof8b.rds")
+saveRDS(pcap_sub31, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof8c.rds")
+saveRDS(pcap_sub32, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof8d.rds")
+
+
+pcap_sub33 <- pcap_sub[8001:9000,1:2000]
+pcap_sub34 <- pcap_sub[8001:9000,2001:4000]
+pcap_sub35 <- pcap_sub[8001:9000,4001:6000]
+pcap_sub36 <- pcap_sub[8001:9000,6001:8095]
+
+saveRDS(pcap_sub33, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof9.rds")
+saveRDS(pcap_sub34, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof9b.rds")
+saveRDS(pcap_sub35, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof9c.rds")
+saveRDS(pcap_sub36, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof9d.rds")
+
+pcap_sub37 <- pcap_sub[9001:10000,1:2000]
+pcap_sub38 <- pcap_sub[9001:10000,2001:4000]
+pcap_sub39 <- pcap_sub[9001:10000,4001:6000]
+pcap_sub40 <- pcap_sub[9001:10000,6001:8095]
+
+saveRDS(pcap_sub37, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof10.rds")
+saveRDS(pcap_sub38, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof10b.rds")
+saveRDS(pcap_sub39, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof10c.rds")
+saveRDS(pcap_sub40, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof10d.rds")
+
+
+pcap_sub41 <- pcap_sub[10001:10200,1:2000]
+pcap_sub42 <- pcap_sub[10001:10200,2001:4000]
+pcap_sub43 <- pcap_sub[10001:10200,4001:6000]
+pcap_sub44 <- pcap_sub[10001:10200,6001:8095]
+
+saveRDS(pcap_sub41, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof11.rds")
+saveRDS(pcap_sub42, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof11b.rds")
+saveRDS(pcap_sub43, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof11c.rds")
+saveRDS(pcap_sub44, "RESULTS/COMPRESSED/GOF/pcap_posterior_gof11d.rds")
+
